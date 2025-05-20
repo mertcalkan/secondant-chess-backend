9c20534c028bb1eef8c746ffe46fbe2e8c6e506c
Mert Calkan <mert.calkan@std.yeditepe.edu.tr>
2025-05-20 17:54:14 +0300

    Update _layout.tsx

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -8,9 +8,9 @@ const TabIcon = ({focused , icon , title} : any) => {
       return (
   <ImageBackground
       source={images.highlight}
-      className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+      className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
     >
-      <Image source={icon} tintColor="#151312" className="size-7" />
+      <Image source={icon} tintColor="#151312" className="size-5" />
       <Text className="text-secondary text-base font-semibold ml-2">{title}</Text>
     </ImageBackground>
 
@@ -18,14 +18,32 @@ const TabIcon = ({focused , icon , title} : any) => {
   }
    return (
     <View className="size-full justify-center items-center mt-2 rounded-full">
-      <Image source={icon} tintColor="#A8B5DB" className="size-6" />
+      <Image source={icon} tintColor="#A8B5DB" className="size-5" />
     </View>
   );
 
 };
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs screenOptions={{
+      tabBarShowLabel : false,
+      tabBarItemStyle : {width : "100%",
+      height : "100%",
+      justifyContent : "center",
+      alignItems : "center"
+      },
+       tabBarStyle: {
+         backgroundColor: "#0F0D23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0F0D23",
+        },
+    }}>
       <Tabs.Screen
         name="index"
         options={{
