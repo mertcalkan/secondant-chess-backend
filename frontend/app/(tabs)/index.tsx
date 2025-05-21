dcc334cc2cfbb7544af49b906754158b0b9d4506
Mert Calkan <mert.calkan@std.yeditepe.edu.tr>
2025-05-21 03:59:06 +0300

    Home UI updates

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,11 +1,36 @@
-import { Text, View } from "react-native";
+import {
+  Image,
+  ScrollView,
+  View
+} from "react-native";
+
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { useRouter } from "expo-router";
+
 export default function Index() {
+  const router = useRouter();
   return (
-    <View className="flex-1 justify-center items-center "
-      
-    >
-      <Text className = "text-5xl text-primary font-bold">Welcome!</Text>
-      
+  <View className="flex-1 bg-primary">
+    
+        <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+
+        <Image source={icons.secondantLogo} className="w-48 h-24 mt-20 mb-5 mx-auto" />
+        
+         <View className="flex-1 mt-5">
+         <SearchBar
+              // onPress={() => {
+              //   router.push("/search");
+              // }}
+              placeholder="Enter a player or keyword to search"
+            />
+      </View>
+      </ScrollView>
+     
     </View>
   );
 }
--- frontend/constants/icons.ts
+++ frontend/constants/icons.ts
@@ -1,3 +1,4 @@
+import secondantLogo from "@/assets/icons/appLogo.png";
 import arrow from "@/assets/icons/arrow.png";
 import chessboard from "@/assets/icons/chessboard.png";
 import chessking from "@/assets/icons/chessking.png";
@@ -9,6 +10,7 @@ import puzzle from "@/assets/icons/puzzle.png";
 import save from "@/assets/icons/save.png";
 import search from "@/assets/icons/search.png";
 import star from "@/assets/icons/star.png";
+
 export const icons = {
   home,
   search,
@@ -20,5 +22,6 @@ export const icons = {
   arrow,
   chessboard,
   puzzle,
-  chessking
+  chessking,
+  secondantLogo
 };
